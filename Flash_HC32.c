451348a5bfbff1648ff3b96d6584a40646f17fb1
thtfcccj <thtfcccj@qq.com>
2020-06-29 09:56:48 +0800

    Flash_HC32.c: 增加对多组Sector时的支持（2K为1个扇区时，最多支持至1M）

--- Flash_HC32.c
+++ Flash_HC32.c
@@ -16,7 +16,7 @@ unsigned long Flash_ErrCount = 0; //失败计数器
 不可在此修改以免影响一致性！
 ****************************************************************************/
 
-//#define SUPPORT_MDK   //Keil MDK环境时，需定义
+//#define SUPPORT_MDK   //Keil MDK环境时，需全局定义
 
 //注：SYS_MHZ为全局定义
 #define _DELAY_MUTI        ((SYS_MHZ + 3) / 4)   //4M为基准插入延时
@@ -26,12 +26,12 @@ unsigned long Flash_ErrCount = 0; //失败计数器
   #define FLASH_BASE   0x00000000 //默认为0
 #endif
 
+#ifndef FLASH_CAPABILITY //FALSH容量
+  #define FLASH_CAPABILITY   0x00010000 //默认为64K
+#endif
+
 #ifndef FLASH_SLOCK_SIZE //每个SLOCK保护的范围
-  #ifdef FLASH_COUNT
-    #define FLASH_SLOCK_SIZE   （FLASH_COUNT / 32） //分为32份
-  #else
-    #define FLASH_SLOCK_SIZE   2048    //默认2K一份
-  #endif
+  #define FLASH_SLOCK_SIZE   2048    //默认2K一份
 #endif
 
 /***************************************************************************
@@ -86,7 +86,68 @@ static void _WaitDone(unsigned short Time)//IAR环境定义
 
 #endif //_DELAY_MUTI
 
-
+//-----------------------------Sector保护处理-------------------------------------
+static void _ProtSector(unsigned long Adr,     //Flash地址
+                         signed char IsCancel)  //0保护，1取消  
+{
+  unsigned short Offset = (Adr - FLASH_BASE) / FLASH_SLOCK_SIZE;
+  unsigned long ProtFlag;
+  if(IsCancel) ProtFlag = 0; //保护寄存器全部写0
+  else ProtFlag  = 1 << (Offset & 0x1f);//取消时对应位写1
+  
+  #if ((FLASH_CAPABILITY / FLASH_SLOCK_SIZE) <= 32)//一页即写下了
+    _WrProtReg(&FLASH->SLOCK, ProtFlag);
+  #else //多页时,最大2K为单位时，已至1M
+    switch(Offset >> 5){ //找到对应组寄存器(排列不连续)
+      case 0: _WrProtReg(&FLASH->SLOCK0, ProtFlag); break;
+      case 1: _WrProtReg(&FLASH->SLOCK1, ProtFlag); break;
+      #if ((FLASH_CAPABILITY / FLASH_SLOCK_SIZE) > (2 * 32))//第2组
+      case 2: _WrProtReg(&FLASH->SLOCK2, ProtFlag); break;
+      #endif
+      #if ((FLASH_CAPABILITY / FLASH_SLOCK_SIZE) > (3 * 32))//第3组
+      case 3: _WrProtReg(&FLASH->SLOCK3, ProtFlag); break;
+      #endif
+      #if ((FLASH_CAPABILITY / FLASH_SLOCK_SIZE) > (4 * 32))//第4组
+      case 4: _WrProtReg(&FLASH->SLOCK4, ProtFlag); break;
+      #endif
+      #if ((FLASH_CAPABILITY / FLASH_SLOCK_SIZE) > (5 * 32))//第5组
+      case 5: _WrProtReg(&FLASH->SLOCK5, ProtFlag); break;
+      #endif
+      #if ((FLASH_CAPABILITY / FLASH_SLOCK_SIZE) > (6 * 32))//第6组
+      case 6: _WrProtReg(&FLASH->SLOCK6, ProtFlag); break;
+      #endif
+      #if ((FLASH_CAPABILITY / FLASH_SLOCK_SIZE) > (7 * 32))//第7组
+      case 7: _WrProtReg(&FLASH->SLOCK7, ProtFlag); break;
+      #endif
+      #if ((FLASH_CAPABILITY / FLASH_SLOCK_SIZE) > (8 * 32))//第8组
+      case 8: _WrProtReg(&FLASH->SLOCK8, ProtFlag); break;
+      #endif
+      #if ((FLASH_CAPABILITY / FLASH_SLOCK_SIZE) > (9 * 32))//第9组
+      case 9: _WrProtReg(&FLASH->SLOCK9, ProtFlag); break;
+      #endif
+      #if ((FLASH_CAPABILITY / FLASH_SLOCK_SIZE) > (10 * 32))//第10组
+      case 10: _WrProtReg(&FLASH->SLOCK10, ProtFlag); break;
+      #endif
+      #if ((FLASH_CAPABILITY / FLASH_SLOCK_SIZE) > (11 * 32))//第11组
+      case 11: _WrProtReg(&FLASH->SLOCK11, ProtFlag); break;
+      #endif
+      #if ((FLASH_CAPABILITY / FLASH_SLOCK_SIZE) > (12 * 32))//第12组
+      case 12: _WrProtReg(&FLASH->SLOCK12, ProtFlag); break;
+      #endif
+      #if ((FLASH_CAPABILITY / FLASH_SLOCK_SIZE) > (13 * 32))//第13组
+      case 13: _WrProtReg(&FLASH->SLOCK13, ProtFlag); break;
+      #endif
+      #if ((FLASH_CAPABILITY / FLASH_SLOCK_SIZE) > (14 * 32))//第14组
+      case 14: _WrProtReg(&FLASH->SLOCK14, ProtFlag); break;
+      #endif
+      #if ((FLASH_CAPABILITY / FLASH_SLOCK_SIZE) > (15 * 32))//第15组
+      case 15: _WrProtReg(&FLASH->SLOCK15, ProtFlag); break;
+      #endif
+      default: break;
+    }
+  #endif//多页时
+}
+                     
 /***************************************************************************
                         标准接口函数实现
 ****************************************************************************/
@@ -128,11 +189,11 @@ void Flash_ErasePage(unsigned long Adr)@".Flash_ErasePage"
 {
   __disable_irq();
   _WrProtReg(&FLASH->CR, FLASH->CR | 2);  //进入页擦除模式
-  _WrProtReg(&FLASH->SLOCK, 1 << ((Adr - FLASH_BASE) / FLASH_SLOCK_SIZE));//取消页保护
+  _ProtSector(Adr, 1);//取消扇区保护
   *(volatile unsigned char*)Adr = 0xff; //写入任意数据启动
   _WaitDone(21000);   //等待操作结束,>20ms为经验值
   _WrProtReg(&FLASH->CR, FLASH->CR & ~0x03);  //退出页擦除模式
-  _WrProtReg(&FLASH->SLOCK, 0);//加上页保护
+  _ProtSector(Adr, 0);  //加上扇区保护
   __enable_irq();
 }
 
@@ -153,7 +214,7 @@ void Flash_Write(unsigned long Adr,   //Flash地址
   //写前准备：
   __disable_irq();
   _WrProtReg(&FLASH->CR, FLASH->CR | 1);  //进入写模式
-  _WrProtReg(&FLASH->SLOCK, 1 << ((Adr - FLASH_BASE) / FLASH_SLOCK_SIZE));//取消页保护
+  _ProtSector(Adr, 1);//取消扇区保护
   
   const unsigned char *pData = (const unsigned char *)pVoid;
   //写入地址没4字对齐时，先一字一字写入以对齐
@@ -178,14 +239,14 @@ void Flash_Write(unsigned long Adr,   //Flash地址
   
   _EndWrPro: //结束写处理
     _WrProtReg(&FLASH->CR, FLASH->CR & ~0x03);  //取消写模式
-    _WrProtReg(&FLASH->SLOCK, 0);//加上页保护
+  _ProtSector(Adr, 0);  //加上扇区保护
     __enable_irq();
 }
 
 //----------------------------从Flash中读取数据实现----------------------
 //此函数为可选功能,可不实现
 void Flash_Read(unsigned long Adr,   //Flash地址
-                void *pVoid,        //要读出的数据
+                void *pVoid,          //要读出的数据
                 unsigned long Len)   //读数据长度
 {
   memcpy(pVoid, (unsigned char*)Adr, Len);
